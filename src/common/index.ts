3f82d1ff81855ff43e4baaade62823ce6c72aba3
Devjoyti Barman <53661347+Devjoyti-Barman@users.noreply.github.com>
2024-03-20 06:12:33 +0000

    remvoed the commented code

--- src/common/index.ts
+++ src/common/index.ts
@@ -2,19 +2,6 @@ import { workspace, Uri, window } from 'vscode';
 import * as path from "path";
 
 const outputChannel = window.createOutputChannel("vsfc");
-/*
-const createFileOnLocal = async (localPath: string): Promise<void> => {
-    const localFilePath = Uri.file(localPath).with({ scheme: "vscode-local" });
-    const localFileDirectory = Uri.file(path.dirname(localPath)).with({ scheme: "vscode-local" });
-
-    try {
-        await workspace.fs.stat(localFilePath);
-    } catch (error) {
-        await workspace.fs.createDirectory(localFileDirectory);
-        await workspace.fs.writeFile(localFilePath, Buffer.from(''));
-    }
-}
-*/
 
 const createFile = async (filePath: Uri): Promise<void> => {
     const fileDirectory = Uri.file(path.dirname(filePath.path)).with({scheme: filePath.scheme});
@@ -27,17 +14,7 @@ const createFile = async (filePath: Uri): Promise<void> => {
         await workspace.fs.writeFile(filePath, Buffer.from(''));
     }
 }
-/*
-const syncFileOn = async (path1_filePath: Uri, path2_filePath: Uri): Promise <void> => {
-    try {
-        const path1_FileData = await workspace.fs.readFile(path1_filePath);
-        await workspace.fs.writeFile(path2_filePath, path1_FileData);
-    } catch (error) {
-        outputChannel.appendLine("Getting error on trying to sync.");
-        outputChannel.appendLine(JSON.stringify(error));
-    }
-}
-*/
+
 const writeFile = async (path1_filePath: Uri, path2_filePath: Uri): Promise <void> => {
     try {
         const path1_FileData = await workspace.fs.readFile(path1_filePath);
